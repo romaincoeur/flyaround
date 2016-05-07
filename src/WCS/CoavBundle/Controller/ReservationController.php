b4729900853595391db94b2af89315241f2e1462
Romain Coeur <coeurro@gmail.com>
2016-05-07 12:10:28 +0200

    q_crud

---
+++ src/WCS/CoavBundle/Controller/ReservationController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace WCS\CoavBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use WCS\CoavBundle\Entity\Reservation;
+use WCS\CoavBundle\Form\ReservationType;
+
+/**
+ * Reservation controller.
+ *
+ */
+class ReservationController extends Controller
+{
+    /**
+     * Lists all Reservation entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reservations = $em->getRepository('WCSCoavBundle:Reservation')->findAll();
+
+        return $this->render('reservation/index.html.twig', array(
+            'reservations' => $reservations,
+        ));
+    }
+
+    /**
+     * Creates a new Reservation entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm('WCS\CoavBundle\Form\ReservationType', $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reservation);
+            $em->flush();
+
+            return $this->redirectToRoute('reservation_show', array('id' => $reservation->getId()));
+        }
+
+        return $this->render('reservation/new.html.twig', array(
+            'reservation' => $reservation,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Reservation entity.
+     *
+     */
+    public function showAction(Reservation $reservation)
+    {
+        $deleteForm = $this->createDeleteForm($reservation);
+
+        return $this->render('reservation/show.html.twig', array(
+            'reservation' => $reservation,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Reservation entity.
+     *
+     */
+    public function editAction(Request $request, Reservation $reservation)
+    {
+        $deleteForm = $this->createDeleteForm($reservation);
+        $editForm = $this->createForm('WCS\CoavBundle\Form\ReservationType', $reservation);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reservation);
+            $em->flush();
+
+            return $this->redirectToRoute('reservation_edit', array('id' => $reservation->getId()));
+        }
+
+        return $this->render('reservation/edit.html.twig', array(
+            'reservation' => $reservation,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Reservation entity.
+     *
+     */
+    public function deleteAction(Request $request, Reservation $reservation)
+    {
+        $form = $this->createDeleteForm($reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($reservation);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('reservation_index');
+    }
+
+    /**
+     * Creates a form to delete a Reservation entity.
+     *
+     * @param Reservation $reservation The Reservation entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Reservation $reservation)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('reservation_delete', array('id' => $reservation->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
